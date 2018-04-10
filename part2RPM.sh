c46ed24de0c9141fa205287939096a40788d7523
EdgeDauDrex <egd34@drexel.edu>
2018-04-10 16:35:43 -0400

    RPM neo4j install fix

--- part2RPM.sh
+++ part2RPM.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 echo part 2: installing neo4j start
 echo downloading neo4j
-sudo rpm --import http://debian.neo4j.org/neotechnology.gpg.key
-sudo bash -c 'cat <<EOF>  /etc/yum.repos.d/neo4j.repo
-[neo4j]
-name=Neo4j RPM Repository
-baseurl=http://yum.neo4j.org/stable
-enabled=1
-gpgcheck=1
-EOF'
-sudo yum install neo4j-2.1.5
+#sudo rpm --import http://debian.neo4j.org/neotechnology.gpg.key
+#sudo bash -c 'cat <<EOF>  /etc/yum.repos.d/neo4j.repo
+#[neo4j]
+#name=Neo4j RPM Repository
+#baseurl=http://yum.neo4j.org/stable
+#enabled=1
+#gpgcheck=1
+#EOF'
+#sudo yum install neo4j-2.1.5
+wget https://neo4j.com/artifact.php?name=neo4j-community-2.1.5-unix.tar.gz
+tar artifact.php?name=neo4j-community-2.1.5-unix.tar.gz
 echo part 2: installing neo4j end
