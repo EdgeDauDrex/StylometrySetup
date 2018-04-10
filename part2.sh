b5bb2839c4a9cb68cbd584dd672e1aba4b1a8e28
EdgeDauDrex <egd34@drexel.edu>
2018-04-10 15:00:52 -0400

    Updated neo4j installer

--- part2.sh
+++ part2.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-echo part 2 start
-echo fill here
-echo part 2 end
+echo part 2: installing neo4j start
+echo downloading neo4j
+wget -O - https://debian.neo4j.org/neotechnology.gpg.key | sudo apt-key add -
+echo 'deb http://debian.neo4j.org/repo stable/' | sudo tee -a /etc/apt/sources.list.d/neo4j.list
+sudo apt-get update
+sudo apt-get install neo4j=2.1.5
+echo downloading gremlin plugin
+git clone https://github.com/EdgeDauDrex/CodeStylometry-missing-joern-tools-files.git
+echo part 2: installing neo4j end
