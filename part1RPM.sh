cbd9883ae53f4091c61b4de260ef30b3c936daff
EdgeDauDrex <egd34@drexel.edu>
2018-04-10 15:42:58 -0400

    Two versions (Debian and RPM) available

---
+++ part1RPM.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo part 1: downloading Joern start
+echo building joern
+wget https://github.com/fabsx00/joern/archive/0.3.1.tar.gz
+tar xfzv 0.3.1.tar.gz
+cd joern-0.3.1
+wget http://mlsec.org/joern/lib/lib.tar.gz
+tar xfzv lib.tar.gz
+ant
+# ~/.bashrc
+alias joern='java -jar $JOERN/bin/joern.jar'
+ant tools
+sudo apt-get install python-setuptools python-dev
+wget https://github.com/fabsx00/python-joern/archive/0.3.1.tar.gz
+tar xfzv 0.3.1.tar.gz
+cd python-joern-0.3.1
+sudo python2 setup.py install
+echo part 1: downloading joern done
---
+++ part3RPM.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo Git downloads start
+echo downloading sorce code stylometry stuff from github fork of aylines stuff
+git clone https://github.com/EdgeDauDrex/CodeStylometry.git
+echo downloading gremlin plugin
+git clone https://github.com/EdgeDauDrex/CodeStylometry-missing-joern-tools-files.git
+echo Git downloads end
