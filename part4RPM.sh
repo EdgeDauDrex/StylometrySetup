f377ae19122efad739748aeda92e7cf2f472c2b8
EdgeDauDrex <egd34@drexel.edu>
2018-04-10 17:05:29 -0400

    Split part4

---
+++ part4RPM.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo starting to install joern tools
+sudo apt-get install graphviz libgraphviz-dev
+git clone https://github.com/fabsx00/joern-tools
+cd joern-tools
+sudo python2 setup.py install
+echo veryify correct install by :
+joern-lookup
+echo ending joern tools install
