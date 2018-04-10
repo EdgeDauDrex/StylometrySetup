41ae995085a4da31ed4e2f94735513e66d90ceaf
Aviel Stein <stein@susqu.edu>
2018-04-10 13:27:08 -0600

    Add files via upload

--- part3.sh
+++ part3.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-echo part 3 start
-echo fill here
-echo part 3 end
+echo Git downloads start
+echo downloading sorce code stylometry stuff from github fork of aylines stuff
+git clone https://github.com/EdgeDauDrex/CodeStylometry.git
+echo downloading gremlin plugin
+git clone https://github.com/EdgeDauDrex/CodeStylometry-missing-joern-tools-files.git
+echo Git downloads end
