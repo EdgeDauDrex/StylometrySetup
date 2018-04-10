6c70b778dc3898261fab444b58a1cdbacac11f7b
EdgeDauDrex <egd34@drexel.edu>
2018-04-10 15:08:36 -0400

    Removed gremlin plugin part

--- part2.sh
+++ part2.sh
@@ -5,6 +5,4 @@ wget -O - https://debian.neo4j.org/neotechnology.gpg.key | sudo apt-key add -
 echo 'deb http://debian.neo4j.org/repo stable/' | sudo tee -a /etc/apt/sources.list.d/neo4j.list
 sudo apt-get update
 sudo apt-get install neo4j=2.1.5
-echo downloading gremlin plugin
-git clone https://github.com/EdgeDauDrex/CodeStylometry-missing-joern-tools-files.git
 echo part 2: installing neo4j end
