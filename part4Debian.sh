30363a1ff367f85ad89559f803aaa42f34c6ea0e
EdgeDauDrex <egd34@drexel.edu>
2018-04-13 15:19:52 -0400

    another dependency fix

--- part4Debian.sh
+++ part4Debian.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo starting to install joern tools
-sudo apt-get install graphviz libgraphviz-dev
+sudo apt-get install graphviz libgraphviz-dev pkg-config
 wget https://pypi.python.org/packages/25/b1/e44c51b47054ad88aadbe9edcf344bf9b3c61d2d6d15719180ee4d130bcd/pygraphviz-1.4rc1.tar.gz
 tar xfzv pygraphviz-1.4rc1.tar.gz
 cd pygraphviz-1.4rc1
--- part4RPM.sh
+++ part4RPM.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo starting to install joern tools
-sudo yum install graphviz graphviz-devel
+sudo yum install graphviz graphviz-devel pkgconfig
 wget https://pypi.python.org/packages/25/b1/e44c51b47054ad88aadbe9edcf344bf9b3c61d2d6d15719180ee4d130bcd/pygraphviz-1.4rc1.tar.gz
 tar xfzv pygraphviz-1.4rc1.tar.gz
 cd pygraphviz-1.4rc1
