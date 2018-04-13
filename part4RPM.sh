7da2ce133283b3657993e018153fc009a1197413
EdgeDauDrex <egd34@drexel.edu>
2018-04-12 21:34:55 -0400

    Package fixes

--- part4RPM.sh
+++ part4RPM.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo starting to install joern tools
-sudo yum install graphviz libgraphviz-dev
+sudo yum install graphviz libgraphviz-devel python-pygraphviz
 git clone https://github.com/fabsx00/joern-tools
 cd joern-tools
 sudo python2 setup.py install
