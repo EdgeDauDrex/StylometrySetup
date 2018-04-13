9ae3c981dd9c1ff7b2030fa2720d063139c9e59b
EdgeDauDrex <egd34@drexel.edu>
2018-04-13 14:45:07 -0400

    some changes to force python 2.7

--- part1RPM.sh
+++ part1RPM.sh
@@ -12,6 +12,8 @@ alias joern='java -jar $JOERN/bin/joern.jar'
 ant tools
 sudo yum install python27
 sudo yum install python-setuptools python-devel
+sudo yum install python27-devel python27-setuptools
+#sudo yum install python-devel.x86_64
 wget https://pypi.python.org/packages/d0/5b/ce38fbd03cd645ab4f121e7df70964a8baeab5cbbabf22e9ed8abfa1aa17/py2neo-2.0.9.tar.gz
 tar xfzv py2neo-2.0.9.tar.gz
 cd py2neo-2.0.9
@@ -21,5 +23,5 @@ sudo python2 setup.py install
 #cd python-joern-0.3.1
 git clone https://github.com/EdgeDauDrex/python-joern.git
 cd python-joern
-sudo python2 setup.py install
+sudo python27 setup.py install
 echo part 1: downloading joern done
--- part4RPM.sh
+++ part4RPM.sh
@@ -4,10 +4,10 @@ sudo yum install graphviz graphviz-devel
 wget https://pypi.python.org/packages/25/b1/e44c51b47054ad88aadbe9edcf344bf9b3c61d2d6d15719180ee4d130bcd/pygraphviz-1.4rc1.tar.gz
 tar xfzv pygraphviz-1.4rc1.tar.gz
 cd pygraphviz-1.4rc1
-sudo python2 setup.py install
+sudo python27 setup.py install
 git clone https://github.com/fabsx00/joern-tools
 cd joern-tools
-sudo python2 setup.py install
+sudo python27 setup.py install
 echo veryify correct install by :
 joern-lookup
 echo ending joern tools install
