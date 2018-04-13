11ee156d62530c8e4b21359b4d60af1e32492545
EdgeDauDrex <egd34@drexel.edu>
2018-04-13 15:21:54 -0400

    debugging messages added

--- part4RPM.sh
+++ part4RPM.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 echo starting to install joern tools
 sudo yum install graphviz graphviz-devel pkgconfig
+echo installing pygraphviz
 wget https://pypi.python.org/packages/25/b1/e44c51b47054ad88aadbe9edcf344bf9b3c61d2d6d15719180ee4d130bcd/pygraphviz-1.4rc1.tar.gz
 tar xfzv pygraphviz-1.4rc1.tar.gz
 cd pygraphviz-1.4rc1
 sudo python27 setup.py install
+echo pygraphviz installed
 git clone https://github.com/fabsx00/joern-tools
 cd joern-tools
 sudo python27 setup.py install
