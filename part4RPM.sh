ca630cda5fed2aed35b9b1daceaf4cc2e842f3b3
EdgeDauDrex <egd34@drexel.edu>
2018-04-17 14:37:51 -0400

    more fix to make pygraphviz install work

--- part4RPM.sh
+++ part4RPM.sh
@@ -9,15 +9,17 @@ echo starting to install joern tools
 #sudo yum -y install epel-release
 #sudo yum repolist
 #sudo yum install dpkg dpkg-devel dpkg-dev
+sudo yum install gcc
 sudo yum install pkgconfig graphviz graphviz-devel libgraphviz libgraphviz-devel >> downloadReport.txt
 echo installing pygraphviz
-wget https://pypi.python.org/packages/25/b1/e44c51b47054ad88aadbe9edcf344bf9b3c61d2d6d15719180ee4d130bcd/pygraphviz-1.4rc1.tar.gz >> downloadReport.txt
-tar xfzv pygraphviz-1.4rc1.tar.gz >> downloadReport.txt
-cd pygraphviz-1.4rc1
-sudo python27 setup.py install --library-path=/usr/lib64/graphviz >> downloadReport.txt
-#sudo python27 setup.py install
+#wget https://pypi.python.org/packages/25/b1/e44c51b47054ad88aadbe9edcf344bf9b3c61d2d6d15719180ee4d130bcd/pygraphviz-1.4rc1.tar.gz >> downloadReport.txt
+#tar xfzv pygraphviz-1.4rc1.tar.gz >> downloadReport.txt
+git clone https://github.com/EdgeDauDrex/pygraphviz.git
+cd pygraphviz
+#sudo python27 setup.py install --library-path=/usr/lib64/graphviz >> downloadReport.txt
+sudo python27 setup.py install
 echo pygraphviz installed
-git clone https://github.com/fabsx00/joern-tools >> downloadReport.txt
+git clone https://github.com/fabsx00/joern-tools.git >> downloadReport.txt
 cd joern-tools
 sudo python27 setup.py install >> downloadReport.txt
 echo verify correct install by :
