30da359134e723c07f8c639bf0386ff92972c62b
EdgeDauDrex <egd34@drexel.edu>
2018-04-17 14:27:15 -0400

    added pack path for library manually

--- part4RPM.sh
+++ part4RPM.sh
@@ -14,8 +14,8 @@ echo installing pygraphviz
 wget https://pypi.python.org/packages/25/b1/e44c51b47054ad88aadbe9edcf344bf9b3c61d2d6d15719180ee4d130bcd/pygraphviz-1.4rc1.tar.gz >> downloadReport.txt
 tar xfzv pygraphviz-1.4rc1.tar.gz >> downloadReport.txt
 cd pygraphviz-1.4rc1
-#sudo python27 setup.py install --include-path=/usr/include/graphviz --library-path=/usr/lib64/graphviz >> downloadReport.txt
-sudo python27 setup.py install
+sudo python27 setup.py install --library-path=/usr/lib64/graphviz >> downloadReport.txt
+#sudo python27 setup.py install
 echo pygraphviz installed
 git clone https://github.com/fabsx00/joern-tools >> downloadReport.txt
 cd joern-tools
