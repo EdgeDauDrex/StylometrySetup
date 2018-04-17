1513c387d33c8752db39411801594919a9432724
EdgeDauDrex <egd34@drexel.edu>
2018-04-17 13:18:53 -0400

    Added dpkg install for rpm systems to help with pygraphviz install

--- part4RPM.sh
+++ part4RPM.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 echo starting to install joern tools
-sudo yum install graphviz graphviz-devel pkgconfig
+sudo yum -y install epel-release
+sudo yum repolist
+sudo yum install dpkg-devel dpkg-dev
+sudo yum install pkgconfig graphviz graphviz-devel 
 echo installing pygraphviz
 wget https://pypi.python.org/packages/25/b1/e44c51b47054ad88aadbe9edcf344bf9b3c61d2d6d15719180ee4d130bcd/pygraphviz-1.4rc1.tar.gz
 tar xfzv pygraphviz-1.4rc1.tar.gz
