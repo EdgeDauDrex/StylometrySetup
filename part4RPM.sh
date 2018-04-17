2a455b10aeafd3425470fb62407500708974d2fa
EdgeDauDrex <egd34@drexel.edu>
2018-04-17 14:06:43 -0400

    trying to figure out problem for graphviz location

--- part4RPM.sh
+++ part4RPM.sh
@@ -6,16 +6,16 @@ echo Aviel93
 echo Aviel93
 echo Aviel93
 echo starting to install joern tools
-sudo yum -y install epel-release
-sudo yum repolist
-sudo yum install dpkg dpkg-devel dpkg-dev
+#sudo yum -y install epel-release
+#sudo yum repolist
+#sudo yum install dpkg dpkg-devel dpkg-dev
 sudo yum install pkgconfig graphviz graphviz-devel >> downloadReport.txt
 echo installing pygraphviz
 wget https://pypi.python.org/packages/25/b1/e44c51b47054ad88aadbe9edcf344bf9b3c61d2d6d15719180ee4d130bcd/pygraphviz-1.4rc1.tar.gz >> downloadReport.txt
 tar xfzv pygraphviz-1.4rc1.tar.gz >> downloadReport.txt
 cd pygraphviz-1.4rc1
 #sudo python27 setup.py install --include-path=/usr/include/graphviz --library-path=/usr/lib64/graphviz >> downloadReport.txt
-sudo python27 setup.py install >> downloadReport.txt
+sudo python27 setup.py install
 echo pygraphviz installed
 git clone https://github.com/fabsx00/joern-tools >> downloadReport.txt
 cd joern-tools
