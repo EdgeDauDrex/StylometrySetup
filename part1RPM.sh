303ecfb934d6847887a60fce58740fe1311a7089
EdgeDauDrex <egd34@drexel.edu>
2018-05-09 12:27:03 -0400

    some fixes

--- part1RPM.sh
+++ part1RPM.sh
@@ -7,15 +7,18 @@ tar xfzv 0.3.1.tar.gz >> downloadReport.txt
 cd joern-0.3.1 >> downloadReport.txt
 wget http://mlsec.org/joern/lib/lib.tar.gz >> downloadReport.txt
 tar xfzv lib.tar.gz >> downloadReport.txt
-ant >> downloadReport.txt
+#ant >> downloadReport.txt
+ant
 # ~/.bashrc
 alias joern='java -jar $JOERN/bin/joern.jar' 
-ant tools >> downloadReport.txt
+#ant tools >> downloadReport.txt
+ant tools
 sudo yum install python27 >> downloadReport.txt
 sudo yum install python-setuptools python-devel >> downloadReport.txt
 sudo yum install python27-devel python27-setuptools >> downloadReport.txt
 #sudo yum install python-devel.x86_64
-wget https://pypi.python.org/packages/d0/5b/ce38fbd03cd645ab4f121e7df70964a8baeab5cbbabf22e9ed8abfa1aa17/py2neo-2.0.9.tar.gz >> downloadReport.txt
+wget https://files.pythonhosted.org/packages/d0/5b/ce38fbd03cd645ab4f121e7df70964a8baeab5cbbabf22e9ed8abfa1aa17/py2neo-2.0.9.tar.gz >> downloadReport.txt
+#wget https://pypi.python.org/packages/d0/5b/ce38fbd03cd645ab4f121e7df70964a8baeab5cbbabf22e9ed8abfa1aa17/py2neo-2.0.9.tar.gz >> downloadReport.txt
 tar xfzv py2neo-2.0.9.tar.gz >> downloadReport.txt
 cd py2neo-2.0.9
 sudo python27 setup.py install >> downloadReport.txt
