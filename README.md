07ce9e43ea474a499d51c6210cbf3641c5e42ea6
EdgeDauDrex <egd34@drexel.edu>
2018-06-25 21:41:10 -0400

    Added new script for setting up the binary stylometry pipeline

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # StylometrySetup
 Step-by-step instructions to setup the binary stylometry project:
-1.  Make sure you have the Java JDK installed and that JAVA_HOME points to it
+1.  Make sure you have the Java 8 JDK installed and that JAVA_HOME points to it
 2.  Make sure you have git installed
 3.  Download/clone this project
 4.  Run the appropriate version of the build script (depending on your OS)
---
+++ binaryStylometySetup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+git clone https://github.com/radare/radare2
+cd radare2
+sys/install.sh   # just run this script to update r2 from git
+cd ..
+wget https://services.gradle.org/distributions/gradle-4.8.1-bin.zip?_ga=2.77067735.335273354.1529975705-1487338249.1529975705
+mkdir /opt/gradle
+unzip -d /opt/gradle gradle-4.8.1-bin.zip
+export PATH=$PATH:/opt/gradle/gradle-4.8.1/bin
+wget http://orientdb.com/download.php?email=unknown@unknown.com&file=orientdb-community-2.1.5.tar.gz
+git clone -b llvm-4.0 https://github.com/obfuscator-llvm/obfuscator.git
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release ../obfuscator/
+make -j7
+cd ..
+tar xfzv orientdb-community-2.1.5.tar.gz
+git clone https://github.com/fabsx00/bjoern
+cd bjoern
+gradle deploy
+git clone https://github.com/a0x77n/bjoern-shell
+cd bjoern-shell
+python3 setup.py install
+bjosh
