33d5f960870f43289920e0f9a17929da65b0d07a
EdgeDauDrex <egd34@drexel.edu>
2018-07-09 23:24:45 -0400

    another minor update

--- binaryStylometySetup.sh
+++ binaryStylometySetup.sh
@@ -11,7 +11,7 @@ sys/install.sh
 cd ..
 git clone https://github.com/calaylin/bda.git
 wget https://services.gradle.org/distributions/gradle-4.8.1-bin.zip?_ga=2.77067735.335273354.1529975705-1487338249.1529975705
-sudo mkdir /opt/gradle#
+sudo mkdir /opt/gradle
 unzip -d /opt/gradle gradle-4.8.1-bin.zip
 export PATH=$PATH:/opt/gradle/gradle-4.8.1/bin
 wget http://orientdb.com/download.php?email=unknown@unknown.com&file=orientdb-community-2.1.5.tar.gz
